2ed6077506477f348ac55025acd80efb89f42f45
Sonu Sharma <sonusharmacse88@gmail.com>
2021-07-06 17:29:41 +0530

    Initial commit for loops project

---
+++ src/Loops.java
@@ -0,0 +1,35 @@
+public class Loops {
+    public static void main(String[] args){
+        String name = "sameer";
+        System.out.println(name);
+        String cityname = "Agra";
+        System.out.println(cityname);
+        int no = 22;
+        System.out.println(no);
+        int age = 55;
+        System.out.println(age);
+        double marks =44.33;
+        System.out.println(marks);
+        double price =88.88;
+        System.out.println(price);
+        System.out.println("printing name"   + name);
+        System.out.println("printing age "     + age);
+     //    for(int i=0; i<10;  i++){
+       //     System.out.println("HELLO");
+           // for(int i=1; i<=10; i++){
+           //     System.out.println(2*i);
+          //   }
+               // for(int i=1;i<=10; i++){
+                 //   System.out.println(5*i);
+                   // for(int m=1;m<=10; m++){
+                     //   System.out.println(10*m);
+                        for(int k=5; k>=1; k--){
+                            System.out.println(1*k);
+                        }
+                   // }
+              //  }
+
+
+      //  }
+    }
+}
---
+++ src/loops2.java
@@ -0,0 +1,20 @@
+public class loops2 {
+    public static void main(String[] args){
+        for(int i=1;i<=10; i++){
+            System.out.println("sonu");
+            for(int a=1;a<=10; a++){
+          System.out.println(2*a);
+          for(int s=10; s>=1; s--){
+              System.out.println(1*s);
+              for(int d=1; d<=10; d++){
+                  System.out.println(7*d);
+                  String cityname = "noida";
+                  System.out.println(cityname);
+              }
+          }
+            }
+
+
+        }
+    }
+}
